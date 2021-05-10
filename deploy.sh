a9961a9d23cd727b347250fa78a45d3fe2bf58bc
sennett <qaplwsokedijrfuhtg6>
2021-05-10 11:09:07 +0100

    new deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf dist/*
+node_modules/.bin/parcel build src/index.html --dist-dir dist
+cp static/* dist
+
+rm -rf ../sennett.github.io/*
+cp dist/* ../sennett.github.io
+cd ../sennett.github.io
+git add --all
+git commit -m 'new deploy'
+git push -u origin master
+git commit --allow-empty -m 'Trigger rebuild'
+git push
